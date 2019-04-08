58f2d3dd828b2eeecb62522eb6dbe8fe917f2f20
Manny <coachmannytw@gmail.com>
2019-04-07 22:13:09 -0600

    CRD - no update yet

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const monk = require("monk");
 // const MongoClient = require("mongodb").MongoClient;
 // const ObjectId = require("mongodb").ObjectID;
 const url =
-  "mongodb://helioadmin:password1234@cluster0-shard-00-00-lbz6b.mongodb.net:27017,cluster0-shard-00-01-lbz6b.mongodb.net:27017,cluster0-shard-00-02-lbz6b.mongodb.net:27017/inventory?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 
 // const db is how you set your database path. Here we use monk to
 // talk to our mongo atlas database and console log that there is a connection
