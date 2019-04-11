d8bd0ca189db70c7a959c554b0a13a593565fd9a
Manny <coachmannytw@gmail.com>
2019-04-10 22:15:39 -0600

    updated put

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const bodyParser = require("body-parser");
 const monk = require("monk");
 // const MongoClient = require("mongodb").MongoClient;
 // const ObjectId = require("mongodb").ObjectID;
-const url =
+const url = "mongodb://helioadmin:password1234@cluster0-shard-00-00-lbz6b.mongodb.net:27017,cluster0-shard-00-01-lbz6b.mongodb.net:27017,cluster0-shard-00-02-lbz6b.mongodb.net:27017/inventory?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+
 
 // const db is how you set your database path. Here we use monk to
 // talk to our mongo atlas database and console log that there is a connection
@@ -28,17 +29,11 @@ const cart = db.get("cart")
 app.use(cors());
 app.use(bodyParser.json());
 
-
+//Inventory
 app.get("/inventory", async function(req, res) {
   const results = await inventory.find();
   res.status(200).send(results);
 });
-
-app.get("/cart", async function(req, res) {
-  const results = await cart.find();
-  res.status(200).send(results);
-});
-
 
 app.delete("/inventory", async (req, res) => {
   return await inventory.findOneAndDelete((req.body), (err, result) => {
@@ -49,20 +44,37 @@ app.delete("/inventory", async (req, res) => {
   });
   
 });
-//test
-// app.get("/inventory", async function(req, res) {
-//   const results = await inventory.findOne({_id: "req.body.id"}, "_id");
+
+// app.patch("/inventory", async (req, res) => {
+//   return await inventory.findOneAndUpdate((req.body), (err, result) => {
+//     if(err) {
+//       throw err;
+//   }
 //   res.send('DELETE request to homepage')
+//   });
+  
 // });
 
-// app.get("/", async function(req, res) {
-//   const 
-// })
 
 app.post('/inventory',async function (req, res) {
   const results = await inventory.insert(req.body)
   res.status(200).send(results)
 })
+
+app.put('/inventory/:id', async (req, res) => {
+ 
+  const results = await inventory.findOneAndUpdate(req.params.id, req.body)
+  console.log(`i'm updated`)
+  res.status(200).send(results)
+})
+
+//Cart
+
+app.get("/cart", async function(req, res) {
+  const results = await cart.find();
+  res.status(200).send(results);
+});
+
 
 app.post('/cart',async function (req, res) {
   const results = await cart.insert(req.body)
@@ -74,3 +86,5 @@ app.post('/cart',async function (req, res) {
 //   });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+
